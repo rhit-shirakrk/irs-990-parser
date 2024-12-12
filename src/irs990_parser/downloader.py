f4cd8a412f365026ac019666da42fdafe18e9123
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-12-11 20:11:21 -0500

    TDD Step 2: test passes

--- src/irs990_parser/downloader.py
+++ src/irs990_parser/downloader.py
@@ -2,9 +2,12 @@
 Download IRS 990 files and save at specified directory
 """
 
+import os
 import pathlib
 
 
 class IRS990FileDownloader:
     def __init__(self, directory: pathlib.Path) -> None:
-        self.directory = directory
+        if os.path.isdir(directory):
+            self.directory = directory
+        raise NotADirectoryError(f"Invalid directory {directory}")
