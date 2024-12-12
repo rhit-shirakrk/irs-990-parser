f866996794658a9d06fd2dc568cc044a3c50ebfb
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-12-11 20:07:47 -0500

    TDD Step 1: test fails, error not raised

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -1,8 +1,12 @@
-from irs990_parser import downloader
-
 """
 Unit tests for downloading IRS 990 files
 """
+
+import pathlib
+
+import pytest
+
+from irs990_parser import downloader
 
 
 class TestIRS990FileDownload:
@@ -10,4 +14,7 @@ class TestIRS990FileDownload:
         """
         Tests for a non-existent directory to save IRS 990 files
         """
-        file_downloader = downloader.IRS990FileDownloader("BAD_PATH")
+        NON_EXISTENT_DIRECTORY = pathlib.Path("BAD_PATH")
+        with pytest.raises(NotADirectoryError) as excinfo:
+            file_downloader = downloader.IRS990FileDownloader(NON_EXISTENT_DIRECTORY)
+        assert f"Invalid directory {NON_EXISTENT_DIRECTORY}" in str(excinfo.value)
