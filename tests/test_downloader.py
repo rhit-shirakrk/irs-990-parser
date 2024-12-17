6016dccc9b91c9d9862271a9c0bf1d91bd3e56d4
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-12-16 22:31:08 -0500

    added docstrings

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -10,11 +10,15 @@ from irs990_parser import downloader
 
 
 class TestIRS990FileDownload:
-    def test_nonexistent_directory(self):
+    """
+    Unit tests for IRS990FileDownloader class
+    """
+
+    def test_nonexistent_directory(self) -> None:
         """
         Tests for a non-existent directory to save IRS 990 files
         """
         NON_EXISTENT_DIRECTORY = pathlib.Path("BAD_PATH")
         with pytest.raises(NotADirectoryError) as excinfo:
-            file_downloader = downloader.IRS990FileDownloader(NON_EXISTENT_DIRECTORY)
+            downloader.IRS990FileDownloader(NON_EXISTENT_DIRECTORY)
         assert f"Invalid directory {NON_EXISTENT_DIRECTORY}" in str(excinfo.value)
