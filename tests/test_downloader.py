e05074d3ad622b5c45daeadfe49ea7485a613eb6
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-12-16 23:04:07 -0500

    TDD Step 1: test fails

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -3,6 +3,7 @@ Unit tests for downloading IRS 990 files
 """
 
 import pathlib
+from datetime import datetime
 
 import pytest
 
@@ -42,5 +43,20 @@ class TestIRS990FileDownload:
             downloader.IRS990FileDownloader(mock_directory, INVALID_START_YEAR)
         assert (
             f"Invalid start year {INVALID_START_YEAR}. The earliest available year is 2018"
+            in str(excinfo.value)
+        )
+
+    def test_later_than_current_year_start_year_expected_value_error(
+        self, mock_directory: pathlib.Path
+    ):
+        """
+        Tests for a start year later than the current year
+        """
+        current_year = datetime.now().year
+        invalid_start_year = current_year + 1
+        with pytest.raises(ValueError) as excinfo:
+            downloader.IRS990FileDownloader(mock_directory, invalid_start_year)
+        assert (
+            f"Invalid start year {invalid_start_year}. The latest available year is {current_year}"
             in str(excinfo.value)
         )
