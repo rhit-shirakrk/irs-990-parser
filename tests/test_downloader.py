77e65ef429b7b2c781cee9c658a83a35ce43fb99
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-12-16 22:54:54 -0500

    TDD Step 1: test fails, constructor needs to be updated

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -9,12 +9,20 @@ import pytest
 from irs990_parser import downloader
 
 
+@pytest.fixture(scope="session")
+def mock_directory(tmp_path_factory: pytest.TempPathFactory) -> pathlib.Path:
+    """
+    Generate mock directory for usage across multiple tests
+    """
+    return tmp_path_factory.mktemp("mock_directory")
+
+
 class TestIRS990FileDownload:
     """
     Unit tests for IRS990FileDownloader class
     """
 
-    def test_nonexistent_directory(self) -> None:
+    def test_nonexistent_directory_expected_not_a_directory_error(self) -> None:
         """
         Tests for a non-existent directory to save IRS 990 files
         """
@@ -22,3 +30,17 @@ class TestIRS990FileDownload:
         with pytest.raises(NotADirectoryError) as excinfo:
             downloader.IRS990FileDownloader(NON_EXISTENT_DIRECTORY)
         assert f"Invalid directory {NON_EXISTENT_DIRECTORY}" in str(excinfo.value)
+
+    def test_earlier_than_2018_start_year_expected_value_error(
+        self, mock_directory: pathlib.Path
+    ) -> None:
+        """
+        Tests for a start year earlier than 2018
+        """
+        INVALID_START_YEAR = 2017
+        with pytest.raises(ValueError) as excinfo:
+            downloader.IRS990FileDownloader(mock_directory, INVALID_START_YEAR)
+        assert (
+            f"Invalid start year {INVALID_START_YEAR}. The earliest available year is 2018"
+            in str(excinfo.value)
+        )
