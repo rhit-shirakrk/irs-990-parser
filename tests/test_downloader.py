49fafe839fcb80764ab2b3ac12554d13f4aac7c6
Ryan Shiraki <shirakrk@rose-hulman.edu>
2024-12-18 14:52:57 -0500

    TDD Step 2: test passes, will re-write zip links tests

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -15,6 +15,16 @@ class TestIRS990LinkRetriever:
     Unit tests for IRS990LinkRetriever class
     """
 
+    INDEX_LINKS = [
+        "https://apps.irs.gov/pub/epostcard/990/xml/2024/index_2024.csv",
+        "https://apps.irs.gov/pub/epostcard/990/xml/2023/index_2023.csv",
+        "https://apps.irs.gov/pub/epostcard/990/xml/2022/index_2022.csv",
+        "https://apps.irs.gov/pub/epostcard/990/xml/2021/index_2021.csv",
+        "https://apps.irs.gov/pub/epostcard/990/xml/2020/index_2020.csv",
+        "https://apps.irs.gov/pub/epostcard/990/xml/2019/index_2019.csv",
+        "https://apps.irs.gov/pub/epostcard/990/xml/2018/index_2018.csv",
+    ]
+
     YEAR_TO_ZIP_LINKS = {
         2018: [
             "https://apps.irs.gov/pub/epostcard/990/xml/2018/2018_TEOS_XML_CT1.zip",
@@ -155,46 +165,16 @@ class TestIRS990LinkRetriever:
             in str(excinfo.value)
         )
 
-    def test_get_index_csv_links_expected_valid(self) -> None:
+    def test_get_index_csv_links_one_year_expected_valid(self) -> None:
         """
-        Tests for proper fetching of links to index files
+        Tests for proper fetching of links to index files for a single year
         """
-        INDEX_LINKS = [
-            "https://apps.irs.gov/pub/epostcard/990/xml/2024/index_2024.csv",
-            "https://apps.irs.gov/pub/epostcard/990/xml/2023/index_2023.csv",
-            "https://apps.irs.gov/pub/epostcard/990/xml/2022/index_2022.csv",
-            "https://apps.irs.gov/pub/epostcard/990/xml/2021/index_2021.csv",
-            "https://apps.irs.gov/pub/epostcard/990/xml/2020/index_2020.csv",
-            "https://apps.irs.gov/pub/epostcard/990/xml/2019/index_2019.csv",
-            "https://apps.irs.gov/pub/epostcard/990/xml/2018/index_2018.csv",
-        ]
         current_year = datetime.now().year
-        for year in range(constants.EARLIEST_START_YEAR, current_year):
-            irs_link_retriever = link_retriever.IRS990LinkRetriever(year, current_year)
-            assert INDEX_LINKS == irs_link_retriever.get_index_csv_links()
-            INDEX_LINKS.pop()
-
+        reverse_index = 1
         for start_year in range(constants.EARLIEST_START_YEAR, current_year + 1):
-            for end_year in range(start_year, current_year + 1):
-                expected_links = INDEX_LINKS[-(end_year % start_year + 1) :]
-                irs_link_retriever = link_retriever.IRS990LinkRetriever(
-                    start_year, end_year
-                )
-                assert expected_links == irs_link_retriever.get_index_csv_links()
-
-    def test_get_zip_links_expected_valid(self) -> None:
-        """
-        Tests for properly fetching of zip links
-        """
-        current_year = datetime.now().year
-        for test_year in range(constants.EARLIEST_START_YEAR, datetime.now().year + 1):
-            expected_zip_links = [
-                zip_link
-                for year, zip_links in TestIRS990LinkRetriever.YEAR_TO_ZIP_LINKS.items()
-                if year >= test_year
-                for zip_link in zip_links
-            ]
+            expected_link = TestIRS990LinkRetriever.INDEX_LINKS[-reverse_index]
             irs_link_retriever = link_retriever.IRS990LinkRetriever(
-                test_year, current_year
+                start_year, start_year
             )
-            assert irs_link_retriever.get_zip_links() == unordered(expected_zip_links)
+            assert [expected_link] == irs_link_retriever.get_index_csv_links()
+            reverse_index += 1
