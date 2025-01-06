7da982ac82cf39001b2d214add546cec38e12a77
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-05 21:20:29 -0500

    TDD Step 2: test passes

--- src/irs990_parser/extractor.py
+++ src/irs990_parser/extractor.py
@@ -2,7 +2,14 @@
 Download and extract information from IRS files
 """
 
+from irs990_parser import custom_exceptions
+
 
 class IRSExtractor:
     def __init__(self) -> None:
         pass
+
+    def extract_zip(self, url: str) -> None:
+        raise custom_exceptions.InvalidZipFileException(
+            f"URL {url} does not yield a ZIP file"
+        )
