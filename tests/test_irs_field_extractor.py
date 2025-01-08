c86f379af03ec43863203552baf60764360e398f
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-08 07:46:46 -0500

    TDD step 1: test fails, new parameter added

--- tests/test_irs_field_extractor.py
+++ tests/test_irs_field_extractor.py
@@ -2,6 +2,8 @@
 Tests all implementations of IRSFieldExtractor
 """
 
+import pathlib
+
 from irs990_parser import irs_field_extractor
 
 
@@ -12,4 +14,5 @@ class TestIRSFieldExtractor:
 
     def test_ein_extractor_expected_381357951(self) -> None:
         """Tests for valid EIN extraction"""
-        ein_extractor = irs_field_extractor.EINExtractor()
+        ein_file = pathlib.Path("sample_irs_xml_files/ein/contains_ein.xml")
+        ein_extractor = irs_field_extractor.EINExtractor(ein_file)
