27e931836663b2629f6e3deb3b57b3d3fed5feaa
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-08 13:49:37 -0500

    re-writing tests for extractors to accept parsed xml as input

--- tests/test_irs_field_extractor.py
+++ tests/test_irs_field_extractor.py
@@ -2,9 +2,13 @@
 Tests all implementations of IRSFieldExtractor
 """
 
+import os
 import pathlib
 
-from irs990_parser import irs_field_extractor
+import bs4
+import pytest
+
+from irs990_parser import custom_exceptions, irs_field_extractor
 
 
 class TestIRSFieldExtractor:
@@ -12,7 +16,15 @@ class TestIRSFieldExtractor:
     Tests for edge cases in IRS fields
     """
 
+    SAMPLE_FILES_DIR = pathlib.Path("sample_irs_xml_files/")
+
     def test_ein_extractor_expected_381357951(self) -> None:
-        """Tests for valid EIN extraction"""
-        ein_file = pathlib.Path("sample_irs_xml_files/ein/contains_ein.xml")
-        ein_extractor = irs_field_extractor.EINExtractor(ein_file)
+        """Tests for an error when a non-xml file is being parsed"""
+        file_with_ein_path = pathlib.Path(
+            os.path.join(TestIRSFieldExtractor.SAMPLE_FILES_DIR, "contains_ein.xml")
+        )
+        with open(file_with_ein_path, "r", encoding="utf-8") as f:
+            file = f.read()
+            parsed_xml = bs4.BeautifulSoup(file, "xml")
+            ein_extractor = irs_field_extractor.EINEXtractor(parsed_xml)
+            assert ein_extractor.extract() == 381357951
