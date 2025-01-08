10d34044433728863844feba64ea55ee42db0e1e
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-08 14:03:32 -0500

    TDD Step 1: test fails, custom exception not created

--- tests/test_irs_field_extractor.py
+++ tests/test_irs_field_extractor.py
@@ -19,7 +19,7 @@ class TestIRSFieldExtractor:
     SAMPLE_FILES_DIR = pathlib.Path("sample_irs_xml_files/")
 
     def test_ein_extractor_expected_742050021(self) -> None:
-        """Tests for an error when a non-xml file is being parsed"""
+        """Tests for proper extraction of an existing EIN field"""
         file_with_ein_path = pathlib.Path(
             os.path.join(
                 TestIRSFieldExtractor.SAMPLE_FILES_DIR, "ein", "contains_ein.xml"
@@ -30,3 +30,17 @@ class TestIRSFieldExtractor:
             parsed_xml = bs4.BeautifulSoup(file, "xml")
             ein_extractor = irs_field_extractor.EINEXtractor(parsed_xml)
             assert ein_extractor.extract() == "742050021"
+
+    def test_ein_extractor_expected_missing_ein_error(self) -> None:
+        """Tests for an error when an EIN is missing from an IRS form"""
+        file_without_ein_path = pathlib.Path(
+            os.path.join(
+                TestIRSFieldExtractor.SAMPLE_FILES_DIR, "ein", "missing_ein.xml"
+            )
+        )
+        with open(file_without_ein_path, "r", encoding="utf-8") as f:
+            file = f.read()
+            parsed_xml = bs4.BeautifulSoup(file, "xml")
+            with pytest.raises(custom_exceptions.MissingEINError) as excinfo:
+                irs_field_extractor.EINEXtractor(parsed_xml)
+            assert "EIN missing from IRS form" in str(excinfo)
