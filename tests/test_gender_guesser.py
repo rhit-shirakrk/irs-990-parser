c17c1fc7aef73824951a1a4390b0d8d60d2c084f
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-13 04:14:31 -0500

    TDD step 1: test fails

---
+++ tests/test_gender_guesser.py
@@ -0,0 +1,17 @@
+"""
+Tests functionality for guessing gender based off name
+"""
+
+import pathlib
+
+
+class TestGenderGuesser:
+    """Tests gender guessing based off name"""
+
+    PROBABILITY_CSV = pathlib.Path(
+        "../src/irs990_parser/first_name_gender_probabilities.csv"
+    )
+
+    def test_gender_guesser_expected_male(self) -> None:
+        """Tests proper guessing of male name"""
+        gender_guesser = GenderGuesser(TestGenderGuesser.PROBABILITY_CSV)
