8f363956dab75b48e05f9dca47dd1dcc624feebf
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-13 00:45:04 -0500

    TDD Step 1: test fails

--- tests/test_irs_field_extractor.py
+++ tests/test_irs_field_extractor.py
@@ -153,3 +153,20 @@ class TestIRSFieldExtractor:
             with pytest.raises(custom_exceptions.MissingFilerException) as excinfo:
                 org_name_extractor.extract()
             assert f"Filer section missing from file {file_name}" in str(excinfo)
+
+    def test_total_compensation_extractor_expected_122207(self) -> None:
+        """Tests for proper extraction of total compensation"""
+        total_compensation_path = pathlib.Path(
+            os.path.join(
+                TestIRSFieldExtractor.SAMPLE_FILES_DIR,
+                "total_compensation",
+                "compensation.xml",
+            )
+        )
+        with open(total_compensation_path, "r", encoding="utf-8") as f:
+            file = f.read()
+            file_name = os.path.basename(total_compensation_path)
+            parsed_xml = bs4.BeautifulSoup(file, "xml")
+            total_compensation_extractor = (
+                irs_field_extractor.TotalCompensationExtractor(file_name, parsed_xml)
+            )
