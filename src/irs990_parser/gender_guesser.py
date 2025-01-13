f680bf864924090114bc0844f1ede1d3fc49bec3
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-13 04:18:03 -0500

    TDD Step 2: test passes

--- src/irs990_parser/gender_guesser.py
+++ src/irs990_parser/gender_guesser.py
@@ -4,3 +4,13 @@ import pathlib
 class GenderGuesser:
     def __init__(self, file_path: pathlib.Path) -> None:
         self.file_path = file_path
+
+    def guess(self, first_name: str) -> str:
+        """Return guessed gender based off first name
+
+        :param first_name: The first name
+        :type first_name: str
+        :return: M if male, F if female
+        :rtype: str
+        """
+        return "M"
