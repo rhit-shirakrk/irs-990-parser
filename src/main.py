a611993a4f62118c56582ed6835c4415c714d03c
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-15 15:23:54 -0500

    moved extraction to separate variables

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import pathlib
 import tempfile
 
 import bs4
+import tqdm
 
 from irs990_parser import (
     extractor,
@@ -33,6 +34,9 @@ if __name__ == "__main__":
         start_year, end_year
     ).get_zip_links()
 
+    guesser = gender_guesser.GenderGuesser(
+        pathlib.Path("../src/irs990_parser/first_name_gender_probabilities.csv")
+    )
     for url in irs_990_links:
         year = get_year_from_url(url)
         with tempfile.TemporaryDirectory() as temp_dir_path:
@@ -44,17 +48,18 @@ if __name__ == "__main__":
 
             monthly_org_data = []
             for file in os.listdir(irs_990_dir):
-                print(f"Processing file {file}")
                 with open(os.path.join(irs_990_dir, file), "r", encoding="utf-8") as f:
                     xml_file = f.read()
                     file_name = os.path.basename(xml_file)
                     parsed_xml = bs4.BeautifulSoup(xml_file, "xml")
 
-                    ein = irs_field_extractor.EINEXtractor(file_name, parsed_xml)
+                    ein = irs_field_extractor.EINEXtractor(
+                        file_name, parsed_xml
+                    ).extract()
 
                     org_name = irs_field_extractor.OrgNameExtractor(
                         file_name, parsed_xml
-                    )
+                    ).extract()
 
                     total_compensation = irs_field_extractor.TotalCompensationExtractor(
                         file_name, parsed_xml
@@ -73,36 +78,32 @@ if __name__ == "__main__":
                     ceo_compensation_review = (
                         irs_field_extractor.CEOCompensationReviewExtractor(
                             file_name, parsed_xml
-                        )
+                        ).extract()
                     )
 
                     other_compensation_review = (
                         irs_field_extractor.OtherCompensationReviewExtractor(
                             file_name, parsed_xml
-                        )
+                        ).extract()
                     )
 
-                    guesser = gender_guesser.GenderGuesser(
-                        pathlib.Path(
-                            "../src/irs990_parser/first_name_gender_probabilities.csv"
-                        )
-                    )
                     trustee_stuff = irs_field_extractor.TrusteeExtractor(
                         file_name, parsed_xml, guesser
                     )
+                    pct_women = trustee_stuff.calculate_trustee_female_percentage()
 
                     key_employer_stuff = irs_field_extractor.KeyEmployeeExtractor(
                         file_name, parsed_xml, guesser
                     )
 
                     org_info = irs_field_extractor.OrganizationDataModel(
-                        ein=ein.extract(),
-                        instnm=org_name.extract(),
+                        ein=ein,
+                        instnm=org_name,
                         year=year,
                         percentage_women_trustees=trustee_stuff.calculate_trustee_female_percentage(),
                         percentage_women_key_employees=key_employer_stuff.calculate_key_employee_female_percentage(),
-                        ceo_reviewed_compensation=ceo_compensation_review.extract(),
-                        other_reviewed_compensation=other_compensation_review.extract(),
+                        ceo_reviewed_compensation=ceo_compensation_review,
+                        other_reviewed_compensation=other_compensation_review,
                         male_to_female_pay_ratio=key_employer_stuff.calculate_male_to_female_pay_ratio(),
                         president_to_average_pay_ratio=key_employer_stuff.calculate_president_to_average_pay_ratio(),
                     )
