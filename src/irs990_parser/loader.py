48fc9dd98b66a05ca49f15e55d6b11c3ca8bbd4d
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-15 04:49:00 -0500

    TDD step 2: test passes

--- src/irs990_parser/loader.py
+++ src/irs990_parser/loader.py
@@ -2,6 +2,8 @@
 Stores functionality to load data for transfer into a database
 """
 
+import pandas as pd
+
 from irs990_parser import irs_field_extractor
 
 
@@ -21,3 +23,11 @@ class Loader:
         :param organization_data: Extracted data from IRS 990 file
         :type organization_data: irs_field_extractor.OrganizationDataModel
         """
+
+    def get_loaded_records(self) -> int:
+        """Return the total number of records
+
+        :return: Number of records
+        :rtype: int
+        """
+        return 1
