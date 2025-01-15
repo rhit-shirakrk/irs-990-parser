b7eb5081605558408fad2f5502a4b3e3b33cff72
Ryan Shiraki <shirakrk@rose-hulman.edu>
2025-01-15 04:45:39 -0500

    TDD step 2: test passes

--- src/irs990_parser/loader.py
+++ src/irs990_parser/loader.py
@@ -2,6 +2,8 @@
 Stores functionality to load data for transfer into a database
 """
 
+from irs990_parser import irs_field_extractor
+
 
 class Loader:
     """
@@ -10,3 +12,12 @@ class Loader:
 
     def __init__(self) -> None:
         pass
+
+    def load(
+        self, organization_data: irs_field_extractor.OrganizationDataModel
+    ) -> None:
+        """Load organization data to be saved into a database
+
+        :param organization_data: Extracted data from IRS 990 file
+        :type organization_data: irs_field_extractor.OrganizationDataModel
+        """
